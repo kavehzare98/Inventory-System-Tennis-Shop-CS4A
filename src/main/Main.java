a20608f156b0235e134cfd5fb84ee637ef4fad21
Kaveh <kavehzare98@gmail.com>
2024-11-20 19:21:55 -0800

    Did some restructuring by moving SportsApparel to appropriate package (inventory)

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+// File: src/main/Main.java
+package main;
+// import inventory;
+//import manager;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
